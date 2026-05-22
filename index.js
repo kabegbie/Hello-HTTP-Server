66fe53e6ece87d3f77cae8a51eaae2658b025d11
Keith Begbie <keithbegbie@Sarahs-MacBook-Air.local>
2026-05-22 16:18:33 -0500

    Update index server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// write your code here
+const http = require("http");
+
+const port = 8080;
+
+const server = http.createServer(function (request, response) {
+  console.log(request.url);
+
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+
+  response.end("Hello World");
+});
+
+server.listen(port, function () {
+  console.log(`Server is listening on port ${port}`);
+});
